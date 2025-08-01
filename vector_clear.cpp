0530ec266cfe07e44a7f67e2347297e2805fc9c9
shimlatuli01 <140829473+shimlatuli01@users.noreply.github.com>
2025-08-01 21:57:40 +0600

    Add files via upload

---
+++ vector_clear.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ vector<int>v;
+ v.push_back(10);
+ v.push_back(20);
+  v.push_back(30);
+  cout<<v.size()<<endl;
+  v.clear();
+ cout<<v.size()<<endl;
+ cout<<v[0]<<endl;
+ cout<<v[1]<<endl;
+ cout<<v[2]<<endl;
+return 0;
+}
