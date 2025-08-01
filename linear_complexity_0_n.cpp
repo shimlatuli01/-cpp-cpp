02644a88c5386f0be674f5aa46f5f612894bbca2
shimlatuli01 <140829473+shimlatuli01@users.noreply.github.com>
2025-08-01 21:48:34 +0600

    Add files via upload

---
+++ linear_complexity_0_n.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int n;//0(1)
+ cin>>n;//0(1)
+ for(int i=1;i<=n;i+=2){ //0(n)
+    cout<<i<<" ";
+ }
+cout<<endl; //0(1)
+for(int i=1;i<=n;i++){ //0(n)
+    cout<<i<<" ";
+ }
+return 0; //0(1)
+}
+//time com: 0(n)
