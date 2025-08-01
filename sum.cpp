7db121889673ddd02362d72d623246aa7936fa24
shimlatuli01 <140829473+shimlatuli01@users.noreply.github.com>
2025-08-01 21:56:04 +0600

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int n;
+ cin>>n;
+ int sum=0;
+//  for(int i=1;i<=100;i++){
+//     sum+=i;
+//  }
+//using formula
+sum=(n*(n+1)/2);
+ cout<<sum<<endl;
+return 0;
+}
