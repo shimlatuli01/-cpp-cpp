7d8dacfdd747eb47b3e3fb8915cedf3e639b5c65
shimlatuli01 <140829473+shimlatuli01@users.noreply.github.com>
2025-08-01 21:58:03 +0600

    Add files via upload

---
+++ vector_initialization.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+// vector<int>v;  //type 1
+//vector<int>v(3); //type 2
+ //cout<<v.size()<<endl;
+//  vector<int>v(10,-1); //type 3
+//  for(int i=0;i<v.size();i++){
+//     cout<<v[i]<<" ";
+//  }
+//  cout<<v.size()<<endl;
+//  vector<int>v(10,-1); //type 4
+//  vector<int>v2(v);
+//   for(int i=0;i<v2.size();i++){
+//       cout<<v2[i]<<" ";
+//   }
+// int a[5]={1,2,3,4,5}; //type 5
+// vector<int>v(a,a+5);
+// for(int i=0;i<v.size();i++){
+//   cout<<v[i]<<" ";
+// }
+vector<int>v={1,2,3,4};//type 6
+ for(int i=0;i<v.size();i++){
+    cout<<v[i]<<" ";
+ }
+return 0;
+}
